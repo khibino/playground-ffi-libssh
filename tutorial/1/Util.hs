09dda8c414cb54b8375390d4605b3f5a481b37d7
Kei Hibino <ex8k.hibino@gmail.com>
2021-06-02 18:01:31 +0900

    add note.

--- tutorial/1/Util.hs
+++ tutorial/1/Util.hs
@@ -3,5 +3,6 @@ module Util (ialloca) where
 import Foreign (Ptr, Storable (poke), alloca)
 
 
+-- action with pointer to specified immediate-value
 ialloca :: Storable a => a -> (Ptr a -> IO b) -> IO b
 ialloca x act = alloca $ \p -> poke p x >> act p
